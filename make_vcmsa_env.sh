c77fcc0cccc1f97b932936da51e6b52ef1bc71fc
Claire D. McWhite <cmcwhite@della8.princeton.edu>
2022-11-04 18:23:47 -0400

    Update env

--- make_vcmsa_env.sh
+++ make_vcmsa_env.sh
@@ -1,2 +1,6 @@
 
-conda create --name vcmsa_env -c conda-forge -c pytorch -c bioconda transformers bioconda::mafft pytorch::pytorch=1.12.1 pandas biopython faiss seqeval cudatoolkit=11.3 python-igraph matplotlib  huggingface_hub icecream
+conda create --name vcmsa_env -c conda-forge -c pytorch -c bioconda transformers bioconda::mafft pytorch::pytorch=1.12.1 pandas biopython faiss seqeval cudatoolkit=11.3 python-igraph matplotlib  huggingface_hub sentencepiece
+
+# Removing icecream
+
+
