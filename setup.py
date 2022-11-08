8b65bcf3d0e52f08e57b0715bedfa1a0605be51a
Claire D. McWhite <cmcwhite@della8.princeton.edu>
2022-11-08 14:03:41 -0500

    Restructure package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from glob import glob
 from os.path import basename
 from os.path import dirname
@@ -11,6 +11,8 @@ def my_test_suite():
     test_suite = test_loader.discover('tests', pattern='*_test.py')
     return test_suite
 
+print(find_packages(where = 'src'))
+
 setup(name='vcmsa',
       version='0.1.0',
       description='Python library to perform vcmsa',
@@ -19,9 +21,12 @@ setup(name='vcmsa',
       author_email='cmcwhite@princeton.edu',
       license='MIT',
       scripts=['bin/vcmsa'],
-      package_dir={'vcmsa':'src'},
-      packages=['vcmsa'],
-      py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+      package_dir={'':'src'},
+      packages  =find_packages (where = "src"),# (include = ['vcmsa', 'vcmsa.*']),
+      #packages = find_packages(), 
+      #packages = ['vcmsa.' + pkg for pkg in find_packages('src/')],
+      #packages=['vcmsa', 'vcmsa_embed'],
+      #py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
       include_package_data=True,
       test_suite='setup.my_test_suite'
 
