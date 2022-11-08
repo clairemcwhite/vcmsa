aa1019329e51ddb73d640da514f7b6b92cdd224a
Claire D. McWhite <cmcwhite@della8.princeton.edu>
2022-11-08 15:54:38 -0500

    env won't solve with pytorch 1.12.1

--- make_vcmsa_env.sh
+++ make_vcmsa_env.sh
@@ -1,5 +1,5 @@
 
-conda create --name vcmsa_env -c conda-forge -c pytorch -c bioconda transformers bioconda::mafft pytorch::pytorch=1.12.1 pandas biopython faiss seqeval cudatoolkit=11.3 python-igraph matplotlib  huggingface_hub sentencepiece
+conda create --name vcmsa_env -c conda-forge -c pytorch -c bioconda transformers bioconda::mafft pytorch::pytorch pandas biopython faiss seqeval cudatoolkit=11.3 python-igraph matplotlib  huggingface_hub sentencepiece
 
 # Removing icecream
 
